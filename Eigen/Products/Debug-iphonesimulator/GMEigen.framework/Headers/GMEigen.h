08ac6a6172ce04de73987117cdf8e466abaca2b3
杨一凡 <340019109@qq.com>
2018-11-30 13:52:22 +0800

    【GMEigen】Step1：创建Eigen的Framework工程和测试工程

---
+++ Eigen/Products/Debug-iphonesimulator/GMEigen.framework/Headers/GMEigen.h
@@ -0,0 +1,19 @@
+//
+//  GMEigen.h
+//  GMEigen
+//
+//  Created by Chris on 2018/11/30.
+//  Copyright © 2018 杨一凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMEigen.
+FOUNDATION_EXPORT double GMEigenVersionNumber;
+
+//! Project version string for GMEigen.
+FOUNDATION_EXPORT const unsigned char GMEigenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMEigen/PublicHeader.h>
+
+
---
+++ Eigen/Products/Debug-iphonesimulator/build/GMEigen.build/Objects-normal/x86_64/GMEigen_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/yangyifan/Documents/workingcopys/ChrisFrameworks/Eigen/Products/Debug-iphonesimulator/build/GMEigen.build/DerivedSources/GMEigen_vers.c
