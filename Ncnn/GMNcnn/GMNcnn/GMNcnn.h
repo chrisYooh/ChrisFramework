e0174bdaae356166c12f8e9f75f5dbc0604826db
杨一凡 <340019109@qq.com>
2018-11-30 15:35:25 +0800

    【GMNcnn】Step1：添加工程模板

---
+++ Ncnn/GMNcnn/GMNcnn/GMNcnn.h
@@ -0,0 +1,19 @@
+//
+//  GMNcnn.h
+//  GMNcnn
+//
+//  Created by Chris on 2018/11/30.
+//  Copyright © 2018 杨一凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMNcnn.
+FOUNDATION_EXPORT double GMNcnnVersionNumber;
+
+//! Project version string for GMNcnn.
+FOUNDATION_EXPORT const unsigned char GMNcnnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMNcnn/PublicHeader.h>
+
+
---
+++ Ncnn/GMNcnn/GMNcnnTests/GMNcnnTests.m
@@ -0,0 +1,37 @@
+//
+//  GMNcnnTests.m
+//  GMNcnnTests
+//
+//  Created by Chris on 2018/11/30.
+//  Copyright © 2018 杨一凡. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GMNcnnTests : XCTestCase
+
+@end
+
+@implementation GMNcnnTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
